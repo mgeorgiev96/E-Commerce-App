196f9fdd8547d7b592c20448cf1a429283d539a8
Martin Georgiev <mgeorgiev9611@abv.bg>
2020-04-19 17:39:39 +0100

    Updates

---
+++ precache-manifest.adfe83d358e2cbe87de159a85aa26b09.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08b6648c15544bda9e2b4e08ab030243",
+    "url": "/E-Commerce-App/index.html"
+  },
+  {
+    "revision": "a61fd1f542f7b9101d95",
+    "url": "/E-Commerce-App/static/css/main.bc7c8601.chunk.css"
+  },
+  {
+    "revision": "5c69f37f70d1a6f9c21a",
+    "url": "/E-Commerce-App/static/js/2.504bc97c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/E-Commerce-App/static/js/2.504bc97c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a61fd1f542f7b9101d95",
+    "url": "/E-Commerce-App/static/js/main.10fb6f71.chunk.js"
+  },
+  {
+    "revision": "ff99be1f820d1f54851f",
+    "url": "/E-Commerce-App/static/js/runtime-main.699a9462.js"
+  },
+  {
+    "revision": "ffd5616f593f3d14cca9d247b9d06eb0",
+    "url": "/E-Commerce-App/static/media/alexander-rotker-l8p1aWZqHvE-unsplash.ffd5616f.jpg"
+  },
+  {
+    "revision": "e30c4950377335e9a062775b994f082a",
+    "url": "/E-Commerce-App/static/media/chuttersnap-4JHMt29fvj8-unsplash.e30c4950.jpg"
+  },
+  {
+    "revision": "df1e5688519c99bb35b889a4f8d6bba4",
+    "url": "/E-Commerce-App/static/media/clint-bustrillos-EpmJBf-kQRE-unsplash.df1e5688.jpg"
+  },
+  {
+    "revision": "4736fcb540261e11c44b260b07849787",
+    "url": "/E-Commerce-App/static/media/fachry-zella-devandra-bNSdIkCBJOs-unsplash.4736fcb5.jpg"
+  },
+  {
+    "revision": "0fe8ceb2816291cb58821a21baa49817",
+    "url": "/E-Commerce-App/static/media/imani-clovis-LxVxPA1LOVM-unsplash.0fe8ceb2.jpg"
+  },
+  {
+    "revision": "abdfac384515086a56368e5bca3a0e9f",
+    "url": "/E-Commerce-App/static/media/jeremy-alford-EfLwt5Xz5Ek-unsplash.abdfac38.jpg"
+  },
+  {
+    "revision": "0afede251fd971a32d7e81559887db74",
+    "url": "/E-Commerce-App/static/media/linda-xu-fUEP0djb1hA-unsplash.0afede25.jpg"
+  },
+  {
+    "revision": "673459af06723aba3ff352f9ee7ab7ef",
+    "url": "/E-Commerce-App/static/media/maksim-larin-NOpsC3nWTzY-unsplash.673459af.jpg"
+  },
+  {
+    "revision": "0ca7a85655eb4cad9265a8ca77077047",
+    "url": "/E-Commerce-App/static/media/maksim-larin-ezdrvzA1hZw-unsplash.0ca7a856.jpg"
+  },
+  {
+    "revision": "6b8aa5eab136bfcbcbd0bab3fc5b9878",
+    "url": "/E-Commerce-App/static/media/malvestida-magazine-DMl5gG0yWWY-unsplash.6b8aa5ea.jpg"
+  },
+  {
+    "revision": "8496e079264d1abf90ed1838731f7475",
+    "url": "/E-Commerce-App/static/media/mitchel-lensink-1BI-ar91C7U-unsplash.8496e079.jpg"
+  },
+  {
+    "revision": "b6ce78db40a167bb7c3bca97e53525ee",
+    "url": "/E-Commerce-App/static/media/nike-swoosh-logo-nike-logo-material-png-clip-art-thumbnail (1).b6ce78db.png"
+  },
+  {
+    "revision": "3f8599483929f2b62ea079186e176cea",
+    "url": "/E-Commerce-App/static/media/plqml-felipe-pelaquim-6zO5VKogoZE-unsplash.3f859948.jpg"
+  },
+  {
+    "revision": "cad57b02966534cb02a21c8cb3ba7931",
+    "url": "/E-Commerce-App/static/media/regis-hari-bouchard-ZjViL8qxihM-unsplash.cad57b02.png"
+  },
+  {
+    "revision": "6e4bd05e873abdcd249546792fdfa1a1",
+    "url": "/E-Commerce-App/static/media/thomas-serer-Jq3IXFoEeUU-unsplash.6e4bd05e.jpg"
+  },
+  {
+    "revision": "c3862351c758e039d3478ca8df5f02ec",
+    "url": "/E-Commerce-App/static/media/zachary-keimig-wT_vIwtVnjk-unsplash.c3862351.jpg"
+  }
+]);
